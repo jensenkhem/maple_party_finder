8fd492a1b47c89f3fdc96ff6a909bc42f912a408
Jensen <jensenkhem@gmail.com>
2020-12-24 02:30:38 -0700

    Added some links to the static pages and routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/home', to: 'static_pages#home' # Not sure if this is necessary along with the root line below!
+  root 'static_pages#home' # Sets the home page of the app, Ex. localhost:3000/ to the home page!
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
