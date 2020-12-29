915c5cbf14e5bcd0ec6228e2aea99a02495668c0
Jensen <jensenkhem@gmail.com>
2020-12-28 20:42:43 -0700

    deleted some migrations

--- db/migrate/20201227203234_add_col_to_character.rb
+++
@@ -1,6 +0,0 @@
-class AddColToCharacter < ActiveRecord::Migration[6.0]
-  def change
-    add_column :characters, :name, :string, unique: true
-    add_column :characters, :class, :string
-  end
-end
--- db/migrate/20201227203902_fix_col_name_character.rb
+++
@@ -1,5 +0,0 @@
-class FixColNameCharacter < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :characters, :class, :char_class
-  end
-end
